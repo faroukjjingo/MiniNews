d0f3c499113ba1df3726aa7801559ee11e35ec4b
Jjingo Farouk <jjingofarouq@gmail.com>
2025-04-18 06:00:10 -0700

    Update App.js

--- mininews/src/App.js
+++ mininews/src/App.js
@@ -1,28 +1,33 @@
+// src/App.js
 import React from 'react';
-import './App.css';
 import { NewsProvider } from './contexts/NewsContext';
+import useNewsApi from './hooks/useNewsApi';
 import Navbar from './components/Navbar';
 import CountrySelector from './components/CountrySelector';
 import CategorySelector from './components/CategorySelector';
 import SearchBar from './components/SearchBar';
 import HeadlinesList from './components/HeadlinesList';
+import LoadingSpinner from './components/LoadingSpinner';
+import './App.css';
 
-function App() {
+const App = () => {
+  const { headlines, loading, error } = useNewsApi();
+
   return (
     <NewsProvider>
-      <div className="App">
+      <div className="app">
         <Navbar />
-        <main>
-          <div className="controls">
-            <CountrySelector />
-            <CategorySelector />
-            <SearchBar />
-          </div>
-          <HeadlinesList />
-        </main>
+        <div className="controls">
+          <CountrySelector />
+          <CategorySelector />
+          <SearchBar />
+        </div>
+        {loading && <LoadingSpinner />}
+        {error && <p className="error">{error}</p>}
+        {!loading && !error && <HeadlinesList headlines={headlines} />}
       </div>
     </NewsProvider>
   );
-}
+};
 
 export default App;
