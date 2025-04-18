5b460d09f5daa7b37f5a66c6b9d5e05f6a28b3bc
Jjingo Farouk <jjingofarouq@gmail.com>
2025-04-18 09:56:33 -0700

    Update LoadingSpinner.js

--- mininews/src/components/LoadingSpinner.js
+++ mininews/src/components/LoadingSpinner.js
@@ -1,13 +1,19 @@
-// src/components/LoadingSpinner.js
-import React from 'react';
+import styled from 'styled-components';
 
-const LoadingSpinner = () => {
-  return (
-    <div className="loading-spinner">
-      <div className="spinner"></div>
-      <p>Loading...</p>
-    </div>
-  );
-};
+const Spinner = styled.div`
+  border: 4px solid #f3f3f3;
+  border-top: 4px solid #3498db;
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  animation: spin 1s linear infinite;
+  margin: 20px auto;
 
+  @keyframes spin {
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
+  }
+`;
+
+const LoadingSpinner = () => <Spinner />;
 export default LoadingSpinner;
