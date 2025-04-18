44c29a800f1f1ab5044590aa5df32d5512712081
Jjingo Farouk <jjingofarouq@gmail.com>
2025-04-18 10:00:54 -0700

    Update HeadlineCard.js

--- mininews/src/components/HeadlineCard.js
+++ mininews/src/components/HeadlineCard.js
@@ -1,14 +1,45 @@
-// src/components/HeadlineCard.js
-import React from 'react';
+import { Link } from 'react-router-dom';
+import { useNewsContext } from '../contexts/NewsContext';
+import styled from 'styled-components';
+import { FaBookmark, FaRegBookmark } from 'react-icons/fa';
+
+const Card = styled.div`
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  padding: 16px;
+  margin: 10px;
+  max-width: 300px;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 150px;
+  object-fit: cover;
+  border-radius: 4px;
+`;
+
+const Title = styled.h3`
+  font-size: 18px;
+  margin: 10px 0;
+`;
 
 const HeadlineCard = ({ article }) => {
+  const { savedArticles, saveArticle, removeArticle } = useNewsContext();
+  const isSaved = savedArticles.some((a) => a.url === article.url);
+
   return (
-    <div className="headline-card">
-      <h3>{article.title}</h3>
-      <a href={article.url} target="_blank" rel="noopener noreferrer" className="source-link">
-        {article.source.name}
-      </a>
-    </div>
+    <Card>
+      {article.urlToImage && <Image src={article.urlToImage} alt={article.title} />}
+      <Title>
+        <Link to={`/article/${encodeURIComponent(article.url)}`}>{article.title}</Link>
+      </Title>
+      <p>{article.description}</p>
+      <button
+        onClick={() => (isSaved ? removeArticle(article.url) : saveArticle(article))}
+      >
+        {isSaved ? <FaBookmark /> : <FaRegBookmark />}
+      </button>
+    </Card>
   );
 };
 
