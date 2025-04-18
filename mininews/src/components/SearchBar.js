909707f33ddf70c9d2f81e351f9e8acc2aec0d6b
Jjingo Farouk <jjingofarouq@gmail.com>
2025-04-18 05:56:05 -0700

    Update SearchBar.js

--- mininews/src/components/SearchBar.js
+++ mininews/src/components/SearchBar.js
@@ -1,28 +1,15 @@
-import React, { useState } from 'react';
-import { useNews } from '../contexts/NewsContext';
-import { FiSearch } from 'react-icons/fi';
+// src/components/SearchBar.js
+import React from 'react';
 
-const SearchBar = () => {
-  const { setSearchQuery } = useNews();
-  const [inputValue, setInputValue] = useState('');
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setSearchQuery(inputValue);
-  };
-
+const SearchBar = ({ searchQuery, setSearchQuery }) => {
   return (
-    <form onSubmit={handleSubmit} className="search-bar">
-      <input
-        type="text"
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-        placeholder="Search headlines..."
-      />
-      <button type="submit">
-        <FiSearch />
-      </button>
-    </form>
+    <input
+      type="text"
+      value={searchQuery}
+      onChange={(e) => setSearchQuery(e.target.value)}
+      placeholder="Search headlines..."
+      className="search-bar"
+    />
   );
 };
 
