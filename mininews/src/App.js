9d13f9144929ed2cb53b94093fc5b6302a8a52b0
Jjingo Farouk <jjingofarouq@gmail.com>
2025-04-18 06:23:54 -0700

    Update App.js

--- mininews/src/App.js
+++ mininews/src/App.js
@@ -1,32 +1,29 @@
 // src/App.js
 import React from 'react';
-import { NewsProvider } from './contexts/NewsContext';
-import useNewsApi from './hooks/useNewsApi';
 import Navbar from './components/Navbar';
 import CountrySelector from './components/CountrySelector';
 import CategorySelector from './components/CategorySelector';
 import SearchBar from './components/SearchBar';
 import HeadlinesList from './components/HeadlinesList';
 import LoadingSpinner from './components/LoadingSpinner';
+import useNewsApi from './hooks/useNewsApi';
 import './App.css';
 
 const App = () => {
   const { headlines = [], loading = false, error = null } = useNewsApi();
 
   return (
-    <NewsProvider>
-      <div className="app">
-        <Navbar />
-        <div className="controls">
-          <CountrySelector />
-          <CategorySelector />
-          <SearchBar />
-        </div>
-        {loading && <LoadingSpinner />}
-        {error && <p className="error">{error}</p>}
-        {!loading && !error && <HeadlinesList headlines={headlines} />}
+    <div className="app">
+      <Navbar />
+      <div className="controls">
+        <CountrySelector />
+        <CategorySelector />
+        <SearchBar />
       </div>
-    </NewsProvider>
+      {loading && <LoadingSpinner />}
+      {error && <p className="error">{error}</p>}
+      {!loading && !error && <HeadlinesList headlines={headlines} />}
+    </div>
   );
 };
 
