cfa09eaae8272ca62dab4cc118c5b9a7e5a1e625
Jjingo Farouk <jjingofarouq@gmail.com>
2025-04-18 05:27:27 -0700

    Update constants.js

---
+++ mininews/src/utils/constants.js
@@ -0,0 +1,12 @@
+export const COUNTRIES = [
+  { code: 'us', name: 'United States' },
+  { code: 'gb', name: 'United Kingdom' },
+  { code: 'in', name: 'India' },
+  { code: 'ca', name: 'Canada' },
+  { code: 'au', name: 'Australia' }
+];
+
+export const CATEGORIES = [
+  'business', 'entertainment', 'general',
+  'health', 'science', 'sports', 'technology'
+];
