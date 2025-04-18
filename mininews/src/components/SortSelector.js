047a848e0de5e8cb13e13efdc4331df9abd5d658
Jjingo Farouk <jjingofarouq@gmail.com>
2025-04-18 23:08:12 +0300

    Update SortSelector.js

--- mininews/src/components/SortSelector.js
+++ mininews/src/components/SortSelector.js
@@ -1,24 +1,24 @@
+// SortSelector.js
 import { useNewsContext } from '../contexts/NewsContext';
 import { SORT_OPTIONS } from '../utils/constants';
-import styled from 'styled-components';
-
-const Select = styled.select`
-  padding: 8px;
-  margin: 10px;
-  border-radius: 4px;
-`;
 
 const SortSelector = () => {
   const { sort, setSort } = useNewsContext();
 
   return (
-    <Select value={sort} onChange={(e) => setSort(e.target.value)}>
-      {SORT_OPTIONS.map(({ value, label }) => (
-        <option key={value} value={value}>
-          {label}
-        </option>
-      ))}
-    </Select>
+    <div className="my-4">
+      <select 
+        value={sort} 
+        onChange={(e) => setSort(e.target.value)}
+        className="w-full py-2 px-3 bg-transparent border border-gray-700 rounded-md text-sm font-medium focus:outline-none focus:ring-1 focus:ring-gray-600 text-gray-200"
+      >
+        {SORT_OPTIONS.map(({ value, label }) => (
+          <option key={value} value={value}>
+            {label}
+          </option>
+        ))}
+      </select>
+    </div>
   );
 };
 
