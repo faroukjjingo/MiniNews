5f2d62910590e5c4a52544df68544de829e28e16
Jjingo Farouk <jjingofarouq@gmail.com>
2025-04-18 05:54:40 -0700

    Update CountrySelector.js

--- mininews/src/components/CountrySelector.js
+++ mininews/src/components/CountrySelector.js
@@ -1,23 +1,71 @@
+// src/components/CountrySelector.js
 import React from 'react';
-import { useNews } from '../contexts/NewsContext';
 
-const CountrySelector = () => {
-  const { country, setCountry } = useNews();
+const countries = [
+  { code: 'ae', name: 'United Arab Emirates' },
+  { code: 'ar', name: 'Argentina' },
+  { code: 'at', name: 'Austria' },
+  { code: 'au', name: 'Australia' },
+  { code: 'be', name: 'Belgium' },
+  { code: 'bg', name: 'Bulgaria' },
+  { code: 'br', name: 'Brazil' },
+  { code: 'ca', name: 'Canada' },
+  { code: 'ch', name: 'Switzerland' },
+  { code: 'cn', name: 'China' },
+  { code: 'co', name: 'Colombia' },
+  { code: 'cu', name: 'Cuba' },
+  { code: 'cz', name: 'Czech Republic' },
+  { code: 'de', name: 'Germany' },
+  { code: 'eg', name: 'Egypt' },
+  { code: 'fr', name: 'France' },
+  { code: 'gb', name: 'United Kingdom' },
+  { code: 'gr', name: 'Greece' },
+  { code: 'hk', name: 'Hong Kong' },
+  { code: 'hu', name: 'Hungary' },
+  { code: 'id', name: 'Indonesia' },
+  { code: 'ie', name: 'Ireland' },
+  { code: 'il', name: 'Israel' },
+  { code: 'in', name: 'India' },
+  { code: 'it', name: 'Italy' },
+  { code: 'jp', name: 'Japan' },
+  { code: 'kr', name: 'South Korea' },
+  { code: 'lt', name: 'Lithuania' },
+  { code: 'lv', name: 'Latvia' },
+  { code: 'ma', name: 'Morocco' },
+  { code: 'mx', name: 'Mexico' },
+  { code: 'my', name: 'Malaysia' },
+  { code: 'ng', name: 'Nigeria' },
+  { code: 'nl', name: 'Netherlands' },
+  { code: 'no', name: 'Norway' },
+  { code: 'nz', name: 'New Zealand' },
+  { code: 'ph', name: 'Philippines' },
+  { code: 'pl', name: 'Poland' },
+  { code: 'pt', name: 'Portugal' },
+  { code: 'ro', name: 'Romania' },
+  { code: 'rs', name: 'Serbia' },
+  { code: 'ru', name: 'Russia' },
+  { code: 'sa', name: 'Saudi Arabia' },
+  { code: 'se', name: 'Sweden' },
+  { code: 'sg', name: 'Singapore' },
+  { code: 'si', name: 'Slovenia' },
+  { code: 'sk', name: 'Slovakia' },
+  { code: 'th', name: 'Thailand' },
+  { code: 'tr', name: 'Turkey' },
+  { code: 'tw', name: 'Taiwan' },
+  { code: 'ua', name: 'Ukraine' },
+  { code: 'us', name: 'United States' },
+  { code: 've', name: 'Venezuela' },
+  { code: 'za', name: 'South Africa' },
+];
 
-  const countries = [
-    { code: 'us', name: 'United States' },
-    { code: 'gb', name: 'United Kingdom' },
-    { code: 'ca', name: 'Canada' },
-    { code: 'au', name: 'Australia' },
-    { code: 'in', name: 'India' },
-  ];
-
+const CountrySelector = ({ country, setCountry }) => {
   return (
-    <select 
-      value={country} 
+    <select
+      value={country}
       onChange={(e) => setCountry(e.target.value)}
       className="country-selector"
     >
+      <option value="">Select a country</option>
       {countries.map((c) => (
         <option key={c.code} value={c.code}>
           {c.name}
