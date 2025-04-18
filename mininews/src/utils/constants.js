9ea7aeec3a34e52c98edca3d655100f123ce1c3a
Jjingo Farouk <jjingofarouq@gmail.com>
2025-04-18 21:03:02 +0300

    Update constants.js

--- mininews/src/utils/constants.js
+++ mininews/src/utils/constants.js
@@ -4,14 +4,39 @@ export const COUNTRIES = [
   { code: 'ca', name: 'Canada' },
   { code: 'au', name: 'Australia' },
   { code: 'in', name: 'India' },
+  { code: 'de', name: 'Germany' },
+  { code: 'fr', name: 'France' },
 ];
 
 export const CATEGORIES = [
+  'general',
   'business',
   'entertainment',
-  'general',
   'health',
   'science',
   'sports',
   'technology',
+];
+
+export const LANGUAGES = [
+  { code: 'en', name: 'English' },
+  { code: 'de', name: 'German' },
+  { code: 'es', name: 'Spanish' },
+  { code: 'fr', name: 'French' },
+  { code: 'it', name: 'Italian' },
+  { code: 'pt', name: 'Portuguese' },
+  { code: 'ru', name: 'Russian' },
+];
+
+export const SORT_OPTIONS = [
+  { value: 'published_desc', label: 'Newest First' },
+  { value: 'published_asc', label: 'Oldest First' },
+  { value: 'popularity', label: 'Most Popular' },
+];
+
+export const SOURCES = [
+  { id: 'cnn', name: 'CNN' },
+  { id: 'bbc', name: 'BBC' },
+  { id: 'abc-au', name: 'ABC Australia' },
+  { id: 'tmz', name: 'TMZ' },
 ];
