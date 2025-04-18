7729fcc063604fae0deb10acad099c0a9804c26a
Jjingo Farouk <jjingofarouq@gmail.com>
2025-04-18 22:57:18 +0300

    Update CountrySelector.js

--- mininews/src/components/CountrySelector.js
+++ mininews/src/components/CountrySelector.js
@@ -1,24 +1,24 @@
+// CountrySelector.js
 import { useNewsContext } from '../contexts/NewsContext';
 import { COUNTRIES } from '../utils/constants';
-import styled from 'styled-components';
-
-const Select = styled.select`
-  padding: 8px;
-  margin: 10px;
-  border-radius: 4px;
-`;
 
 const CountrySelector = () => {
   const { country, setCountry } = useNewsContext();
 
   return (
-    <Select value={country} onChange={(e) => setCountry(e.target.value)}>
-      {COUNTRIES.map(({ code, name }) => (
-        <option key={code} value={code}>
-          {name}
-        </option>
-      ))}
-    </Select>
+    <div className="my-4">
+      <select 
+        value={country} 
+        onChange={(e) => setCountry(e.target.value)}
+        className="w-full py-2 px-3 bg-transparent border border-gray-700 rounded-md text-sm font-medium focus:outline-none focus:ring-1 focus:ring-gray-400 text-gray-200"
+      >
+        {COUNTRIES.map(({ code, name }) => (
+          <option key={code} value={code}>
+            {name}
+          </option>
+        ))}
+      </select>
+    </div>
   );
 };
 
