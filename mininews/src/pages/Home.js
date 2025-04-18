c2eba0fe753035d97abe1cfebb14b7cbc709a489
Jjingo Farouk <jjingofarouq@gmail.com>
2025-04-18 21:06:21 +0300

    Update Home.js

--- mininews/src/pages/Home.js
+++ mininews/src/pages/Home.js
@@ -3,6 +3,10 @@ import { useNewsApi } from '../hooks/useNewsApi';
 import HeadlinesList from '../components/HeadlinesList';
 import CountrySelector from '../components/CountrySelector';
 import CategorySelector from '../components/CategorySelector';
+import SourceSelector from '../components/SourceSelector';
+import LanguageSelector from '../components/LanguageSelector';
+import SortSelector from '../components/SortSelector';
+import DateSelector from '../components/DateSelector';
 import styled from 'styled-components';
 
 const Container = styled.div`
@@ -10,14 +14,35 @@ const Container = styled.div`
   text-align: center;
 `;
 
+const Filters = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 10px;
+`;
+
 const Home = () => {
-  const { country, category, query } = useNewsContext();
-  const { articles, loading, error } = useNewsApi({ country, category, query });
+  const { country, category, query, sources, languages, sort, date } = useNewsContext();
+  const { articles, loading, error } = useNewsApi({
+    country,
+    category,
+    query,
+    sources,
+    languages,
+    sort,
+    date,
+  });
 
   return (
     <Container>
-      <CountrySelector />
-      <CategorySelector />
+      <Filters>
+        <CountrySelector />
+        <CategorySelector />
+        <SourceSelector />
+        <LanguageSelector />
+        <SortSelector />
+        <DateSelector />
+      </Filters>
       <HeadlinesList articles={articles} loading={loading} error={error} />
     </Container>
   );
