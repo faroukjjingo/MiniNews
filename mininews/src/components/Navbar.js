2df66f425025876a12afdf3ad22313d3df5faf14
Jjingo Farouk <jjingofarouq@gmail.com>
2025-04-18 05:57:02 -0700

    Update Navbar.js

--- mininews/src/components/Navbar.js
+++ mininews/src/components/Navbar.js
@@ -1,19 +1,10 @@
+// src/components/Navbar.js
 import React from 'react';
-import { FiGlobe, FiSun, FiMoon } from 'react-icons/fi';
-import { useNews } from '../contexts/NewsContext';
 
 const Navbar = () => {
-  const { darkMode, toggleDarkMode } = useNews();
-
   return (
-    <nav className={`navbar ${darkMode ? 'dark' : ''}`}>
-      <div className="navbar-brand">
-        <FiGlobe className="logo" />
-        <h1>NewsFlash</h1>
-      </div>
-      <button onClick={toggleDarkMode} className="theme-toggle">
-        {darkMode ? <FiSun /> : <FiMoon />}
-      </button>
+    <nav className="navbar">
+      <h1>News Headlines</h1>
     </nav>
   );
 };
