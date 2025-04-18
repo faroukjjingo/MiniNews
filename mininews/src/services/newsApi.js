fc05b56dcbb43f7b48e290b02a51d01e71b94bea
Jjingo Farouk <jjingofarouq@gmail.com>
2025-04-18 09:54:34 -0700

    Update newsApi.js

--- mininews/src/services/newsApi.js
+++ mininews/src/services/newsApi.js
@@ -1,16 +1,37 @@
-// src/services/newsApi.js
 import axios from 'axios';
 
-const newsApi = axios.create({
-  baseURL: 'https://newsapi.org/v2',
-});
+const API_KEY = process.env.REACT_APP_NEWS_API_KEY;
+const BASE_URL = 'https://newsapi.org/v2';
 
-export const fetchTopHeadlines = async (params) => {
-  const response = await newsApi.get('/top-headlines', {
-    params: {
-      ...params,
-      apiKey: process.env.REACT_APP_NEWSAPI_KEY,
-    },
-  });
-  return response.data.articles;
+export const fetchTopHeadlines = async ({ country = 'us', category = '', page = 1, pageSize = 20 }) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/top-headlines`, {
+      params: {
+        country,
+        category,
+        page,
+        pageSize,
+        apiKey: API_KEY,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    throw new Error('Failed to fetch headlines');
+  }
+};
+
+export const searchNews = async ({ query, page = 1, pageSize = 20 }) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/everything`, {
+      params: {
+        q: query,
+        page,
+        pageSize,
+        apiKey: API_KEY,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    throw new Error('Failed to search news');
+  }
 };
