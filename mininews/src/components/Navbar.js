81506affe73b64020baaa4eeb5b2451be1b33a3b
Jjingo Farouk <jjingofarouq@gmail.com>
2025-04-18 10:03:09 -0700

    Update Navbar.js

--- mininews/src/components/Navbar.js
+++ mininews/src/components/Navbar.js
@@ -1,12 +1,32 @@
-// src/components/Navbar.js
-import React from 'react';
+import { Link } from 'react-router-dom';
+import SearchBar from './SearchBar';
+import styled from 'styled-components';
 
-const Navbar = () => {
-  return (
-    <nav className="navbar">
-      <h1>News Headlines</h1>
-    </nav>
-  );
-};
+const Nav = styled.nav`
+  background: #3498db;
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: white;
+`;
+
+const NavLinks = styled.div`
+  a {
+    color: white;
+    margin: 0 15px;
+    text-decoration: none;
+  }
+`;
+
+const Navbar = () => (
+  <Nav>
+    <NavLinks>
+      <Link to="/">Home</Link>
+      <Link to="/saved">Saved</Link>
+    </NavLinks>
+    <SearchBar />
+  </Nav>
+);
 
 export default Navbar;
