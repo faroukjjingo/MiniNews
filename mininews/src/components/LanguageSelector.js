068ff2328e87540cffda876f43d189cc19e0ca19
Jjingo Farouk <jjingofarouq@gmail.com>
2025-04-18 22:59:55 +0300

    Update LanguageSelector.js

--- mininews/src/components/LanguageSelector.js
+++ mininews/src/components/LanguageSelector.js
@@ -1,25 +1,25 @@
+// LanguageSelector.js
 import { useNewsContext } from '../contexts/NewsContext';
 import { LANGUAGES } from '../utils/constants';
-import styled from 'styled-components';
-
-const Select = styled.select`
-  padding: 8px;
-  margin: 10px;
-  border-radius: 4px;
-`;
 
 const LanguageSelector = () => {
   const { languages, setLanguages } = useNewsContext();
 
   return (
-    <Select value={languages} onChange={(e) => setLanguages(e.target.value)}>
-      <option value="">All Languages</option>
-      {LANGUAGES.map(({ code, name }) => (
-        <option key={code} value={code}>
-          {name}
-        </option>
-      ))}
-    </Select>
+    <div className="my-4">
+      <select 
+        value={languages} 
+        onChange={(e) => setLanguages(e.target.value)}
+        className="w-full py-2 px-3 bg-transparent border border-gray-700 rounded-md text-sm font-medium focus:outline-none focus:ring-1 focus:ring-gray-400 text-gray-200"
+      >
+        <option value="">All Languages</option>
+        {LANGUAGES.map(({ code, name }) => (
+          <option key={code} value={code}>
+            {name}
+          </option>
+        ))}
+      </select>
+    </div>
   );
 };
 
