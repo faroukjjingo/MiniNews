d93b55cfdb9ceb8d0532617626b0bb522e45103c
Jjingo Farouk <jjingofarouq@gmail.com>
2025-04-18 06:04:09 -0700

    Update index.js

--- mininews/src/index.js
+++ mininews/src/index.js
@@ -1,18 +1,12 @@
 // src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import { NewsProvider } from './contexts/NewsContext';
+import './index.css';
 
-// Create a root element for React 18's concurrent rendering
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// Render the application with all providers
 root.render(
   <React.StrictMode>
-    <NewsProvider>
-      <App />
-    </NewsProvider>
+    <App />
   </React.StrictMode>
 );
